89d20297fa09b466ee50d9aceae4df1bc8e6ee69
Akshay Solanki <146133555+1solanki1@users.noreply.github.com>
2026-02-22 13:24:23 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 35.6 MB (79.55%)

---
+++ 0724-find-pivot-index/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int leftsum=0;int total=accumulate(nums.begin(),nums.end(),0);
+
+        for(int i=0;i<nums.size();i++){
+      if(leftsum==total-leftsum-nums[i])return i;
+        leftsum+=nums[i];
+    }
+    return -1;
+    }
+};
