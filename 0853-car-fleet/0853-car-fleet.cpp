30172c88cac6fdc30deec9993c32caa3e1bc8e93
Akshay Solanki <146133555+1solanki1@users.noreply.github.com>
2026-07-08 17:17:22 +0530

    Time: 67 ms (26.92%), Space: 118.6 MB (13.06%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int carFleet(int tar, vector<int>& pos, vector<int>& sp) {
+        vector<pair<int,double>>v;
+
+    for(int i=0;i<pos.size();i++){
+        v.push_back({pos[i],(double)(tar-pos[i])/sp[i]});
+        }
+
+ sort(v.rbegin(),v.rend());
+stack<double>st;
+
+    for(int i=0;i<v.size();i++){
+        if(st.empty()||v[i].second>st.top())  st.push(v[i].second);
+        }
+
+return st.size();
+}
+};
